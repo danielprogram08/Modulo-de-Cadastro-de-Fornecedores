8c75fe332f479e3c326cb4abd1a026945daff97f
danielprogram08 <danieldanielsilva08@gmail.com>
2025-01-28 01:17:26 -0300

    Add animation in alerts

--- Front/Register_Supplier/Functions/Alerts/AlertsScript.js
+++ Front/Register_Supplier/Functions/Alerts/AlertsScript.js
@@ -7,19 +7,29 @@ export function alertErrorRegister() {
     alert.role = "alert";
     alert.style.cssText = `
         position: fixed;
-        top: 0;
+        top: -50px;
         width: 100%;
         text-align: center;
+        transition: top 0.5s ease-in, top 0.5s ease-out;
+        
     `;
     alert.innerHTML =
         `𝗘𝗿𝗿𝗼 𝗮𝗼 𝗰𝗮𝗱𝗮𝘀𝘁𝗿𝗮𝗿 𝗳𝗼𝗿𝗻𝗲𝗰𝗲𝗱𝗼𝗿!<br>
         Verifique se este fornecedor já foi cadastrado.`
-    container.style.marginTop = "90px";
+
     document.body.insertBefore(alert, document.body.firstChild);
 
     setTimeout(() => {
-        alert.remove();
-        container.style.marginTop = "0";
+        alert.style.top = "0";
+        container.style.marginTop = "90px";
+    }, 10);
+
+    setTimeout(() => {
+        alert.style.top = "-50px";
+        setTimeout(() => {
+            alert.remove();
+            container.style.marginTop = "0";
+        }, 500);
     }, 3000);
 }
 
@@ -28,22 +38,31 @@ export function alertSucessRegister() {
     let alertSucess = document.createElement("div");
     let container = document.querySelector(".container");
 
-    alertSucess.className = "alert alert-sucess";
+    alertSucess.className = "alert alert-success";
     alertSucess.role = "alert";
     alertSucess.style.cssText = `
         position: fixed;
-        top: 0;
+        top: -50px;
         width: 100%;
         text-align: center;
+        transition: top 0.5s ease-in, top 0.5s ease-out;
     `;
     alertSucess.innerHTML =
         `𝗙𝗼𝗿𝗻𝗲𝗰𝗲𝗱𝗼𝗿 𝗰𝗮𝗱𝗮𝘀𝘁𝗿𝗮𝗱𝗼 𝗰𝗼𝗺 𝘀𝘂𝗰𝗲𝘀𝘀𝗼!`
-    container.style.marginTop = "40px";
-    document.body.insertBefore(alert, document.body.firstChild);
+    
+    document.body.insertBefore(alertSucess, document.body.firstChild);
 
     setTimeout(() => {
-        alertSucess.remove();
-        container.style.marginTop = "0";
+        alertSucess.style.top = "0";
+        container.style.marginTop = "40px";
+    }, 10);
+
+    setTimeout(() => {
+        alertSucess.style.top = "-50px";
+        setTimeout(() => {
+            alertSucess.remove();
+            container.style.marginTop = "0";
+        }, 500);
     }, 3000);
 }
 
@@ -56,18 +75,27 @@ export function alertEmptyFields() {
     alert.role = "alert";
     alert.style.cssText = `
         position: fixed;
-        top: 0;
+        top: -50px;
         width: 100%;
         text-align: center;
+        transition: top 0.5s ease-in, top 0.5s ease-out;
     `;
     alert.innerHTML =
         `𝗣𝗿𝗲𝗲𝗻𝗰𝗵𝗮 𝘁𝗼𝗱𝗼𝘀 𝗼𝘀 𝗰𝗮𝗺𝗽𝗼𝘀!`
-    container.style.marginTop = "40px";
+
     document.body.insertBefore(alert, document.body.firstChild);
 
     setTimeout(() => {
-        alert.remove();
-        container.style.marginTop = "0";
+        alert.style.top = "0";
+        container.style.marginTop = "40px";
+    }, 10);
+
+    setTimeout(() => {
+        alert.style.top = "-50px";
+        setTimeout(() => {
+            alert.remove();
+            container.style.marginTop = "0";
+        }, 500);
     }, 3000);
 }
 
@@ -79,18 +107,27 @@ export function alertErrorSearch() {
     alert.role = "alert";
     alert.style.cssText = `
         position: fixed;
-        top: 0;
+        top: -50px;
         width: 100%;
         text-align: center;
+        transition: top 0.5s ease-in, top 0.5s ease-out;
     `;
     alert.innerHTML =
         `𝗘𝗿𝗿𝗼 𝗮𝗼 𝗰𝗼𝗻𝘀𝘂𝗹𝘁𝗮𝗿 𝗳𝗼𝗿𝗻𝗲𝗰𝗲𝗱𝗼𝗿!<br>
          Verifique se o fornecedor foi cadastrado!`
-    container.style.marginTop = "90px";
+
     document.body.insertBefore(alert, document.body.firstChild);
 
     setTimeout(() => {
-        alert.remove();
-        container.style.marginTop = "0";
+        alert.style.top = "0";
+        container.style.marginTop = "90px";
+    }, 10);
+
+    setTimeout(() => {
+        alert.style.top = "-50px";
+        setTimeout(() => {
+            alert.remove();
+            container.style.marginTop = "0";
+        }, 500);
     }, 3000);
 }
