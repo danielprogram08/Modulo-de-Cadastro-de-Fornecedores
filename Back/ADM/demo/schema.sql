d8581e0fd3dc8a3097c795de95563f5d170e65d4
danielprogram08 <danieldanielsilva08@gmail.com>
2025-02-13 12:41:58 -0300

    Ambient homolog config complete!

--- Back/ADM/demo/schema.sql
+++ Back/ADM/demo/schema.sql
@@ -4,11 +4,11 @@ USE AdmDatabase;
 CREATE TABLE IF NOT EXISTS administrators (
     id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
-    password VARCHAR(16) NOT NULL, UNIQUE
+    password VARCHAR(16) NOT NULL UNIQUE
 );
 
 INSERT INTO administrators (
     name, password
 ) VALUES (
-    'Daniel', 'daniel1234'
+    "Daniel", "daniel1234"
 );
