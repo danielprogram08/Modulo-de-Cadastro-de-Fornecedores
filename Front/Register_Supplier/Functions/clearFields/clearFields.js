2d075db2ccc334b8076bab5f34c5993f6e7c9a64
danielprogram08 <danieldanielsilva08@gmail.com>
2025-12-29 11:31:09 -0300

    Add: Refactoring code to project with React!

--- Front/Register_Supplier/Functions/clearFields/clearFields.js
+++
@@ -1,8 +0,0 @@
-export function clearFields() {
-    document.getElementById("name").value = "";
-    document.getElementById("email").value = "";
-    document.getElementById("address").value = "";
-    document.getElementById("telephone").value = "";
-    document.getElementById("CnpjCpf").value = "";
-    document.getElementById("CorporateReason").value = "";
-}
